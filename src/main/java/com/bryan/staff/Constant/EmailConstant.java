5eaf672ce7778e52364bddbbc2f67268ce7801a0
Bryan-kym <53871996+Bryan-kym@users.noreply.github.com>
2022-04-27 09:03:31 +0300

    version 1.0.1

--- src/main/java/com/bryan/staff/Constant/EmailConstant.java
+++ src/main/java/com/bryan/staff/Constant/EmailConstant.java
@@ -3,10 +3,10 @@ package com.bryan.staff.Constant;
 public class EmailConstant {
 
 	public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-	public static final String USERNAME = "brianyegon99@gmail.com";
-	public static final String PASSWORD = "Kimutaikirui@36635809;";
-	public static final String CC_EMAIL = "brian.kitui@kra.go.ke";
-	public static final String FROM_EMAIL = "brianyegon99@gmail.com";
+	public static final String USERNAME = "youremail";
+	public static final String PASSWORD = "yourpassword";
+	public static final String CC_EMAIL = "email_to_CC";
+	public static final String FROM_EMAIL = "email_to_ssend_mail";
 	public static final String EMAIL_SUBJECT = "KyM ENT - New Password";
 	public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
 	public static final String SMTP_HOST = "mail.smtp.host";
